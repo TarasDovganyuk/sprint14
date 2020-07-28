a53df1737657af3ab90dd607da2539abd795eb08
TarasDovganyuk <exscee@gmail.com>
2020-07-28 23:19:55 +0300

    Add link added

--- src/main/java/com/softserve/edu/controller/MarathonController.java
+++ src/main/java/com/softserve/edu/controller/MarathonController.java
@@ -2,6 +2,7 @@ package com.softserve.edu.controller;
 
 
 import com.softserve.edu.model.Marathon;
+import com.softserve.edu.model.User;
 import com.softserve.edu.service.MarathonService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @Data
 public class MarathonController {
     private MarathonService marathonService;
+
 
     @Autowired
     public void setMarathonService(MarathonService marathonService) {
@@ -35,16 +39,33 @@ public class MarathonController {
     }
 
 
-//    @GetMapping("/addMarathon")
-//    public String addMarathon(Model model) {
-//        return "addMarathon";
-//    }
-//
-//    @PostMapping("/addMarathon")
-//    public String addMarathon(@ModelAttribute(name="marathon") Marathon marathon) {
-//        marathonService.createOrUpdate(marathon);
-//        return "redirect:/marathons";
-//    }
+    @GetMapping("/addMarathon")
+    public String addMarathon(Model model) {
+        Marathon marathon = new Marathon();
+        model.addAttribute("marathon", marathon);
+        return "addMarathon";
+    }
+
+    @PostMapping("/saveMarathon")
+    public String addMarathon(@ModelAttribute(name="marathon") Marathon marathon) {
+        marathonService.createOrUpdate(marathon);
+
+        return "redirect:/marathons";
+    }
+
+    @GetMapping("/users")
+    public String getAllUsers(Model model) {
+//        model.getAttribute("marathon");
+//        Marathon marathon = marathonService.getMarathonById()
+       List<User> users = new ArrayList<>();
+       User user1 = new User();
+       user1.setFirstName("Ivan");
+       users.add(user1);
+        model.addAttribute("users", users);
+        return "users";
+    }
+
+
 
 
 
